d2156678357ad9835c30e2ad1d1458374e895c74
Ilya Sotnikov <ilia12st@yandex.ru>
2024-10-17 18:40:04 +0300

    2

--- VKClient/Program.cs
+++ VKClient/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 class Program
 {
-    private static readonly HttpClient client = new ();
+    private static readonly HttpClient client = new();
 
     static async Task Main(string[] args)
     {
         string accessToken = "6f9ab4036f9ab4036f9ab403796cbbab8366f9a6f9ab403088b966bedb2d7693868fb1b";
         string method = "users.get";
+        string userId = "";
+        userId = Console.ReadLine();
 
-        string url = $"https://api.vk.com/method/{method}?user_ids=1&access_token={accessToken}";
+        string url = $"https://api.vk.com/method/{method}?user_ids={userId}&access_token={accessToken}&v=5.131";
 
-        try
-        {
-            var response = await client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine($"Error: {e.Message}");
-        }
+        var jresponse = await client.GetAsync(url);
+        jresponse.EnsureSuccessStatusCode();
+
+        var content = await jresponse.Content.ReadAsStringAsync();
+
+        var response = JsonSerializer.Deserialize<Response>(content);
+
+        Console.WriteLine($"{response.response[0].last_name}");
+    }
+
+    public class Response
+    {
+        public IList<Content> response { get; set; }
+    }
+
+
+    public class Content
+    {
+        public int id { get; set; }
+        public int bdate { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
     }
 }
